dc237ec2748edd77fb7f48152a30b13c8e34760b
Jared DiCioccio <jared.dicioccio@gmail.com>
2012-05-09 21:27:57 -0400

    Added servercontroller...

---
+++ src/com/mud/server/ServerController.java
@@ -0,0 +1,22 @@
+package com.mud.server;
+
+
+/**
+ * @author Jared DiCioccio
+ * <br><br>
+ * The ServerController parses messages received by Server and acts on them
+ *
+ */
+public class ServerController {	
+	
+	public ServerController(){
+		
+	}
+	
+	public String parse(String input){
+		String output = "";
+		return output;
+	}
+	
+
+}
